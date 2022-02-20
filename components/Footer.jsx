2ec0a1a5a49b673e7d6667df2dab14fc3ee59250
LukaleTyson <lukaletyson99@gmail.com>
2022-02-20 20:27:27 +0300

    tweaked the footer again

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -7,10 +7,11 @@ const Footer=()=>{
             <div className={styles.cardL}>
                 <h1 className={styles.title}>AV0CAD0 CREATIVES.</h1>
                 <h1 className={styles.linkTitle}>
+                    
                     <Link href="/contact" className={styles.link} passHref>
                         <span className={styles.linkText}>WORK WITH US</span>
-                        <Image src="/img/link.png" alt="" height="40px" width="40px" />
                     </Link>
+                    <Image src="/img/link.png" alt="" height="40px" width="40px" />
                 </h1>
             </div>
             <div className={styles.cardS}>
